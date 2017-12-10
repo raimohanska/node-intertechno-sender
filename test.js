7f33ddc4b07460f27319eaab747376fd5dbea330
Juha Paananen <juha.paananen@gmail.com>
2017-12-10 16:33:53 +0200

    add on/off codes for my switches

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-var rcswitch = require('./index.js'); // May throw an error if /dev/gpiomem is not accessible
-rcswitch.enableTransmit(7); // Use **BCM** pin 17
-rcswitch.setRepeatTransmit(15); // Set number of repeat transmissions
-rcswitch.send('1001011001101001010101011010010101010101010101010110100101010101')
+var sender = require('./index.js'); // May throw an error if /dev/gpiomem is not accessible
+
+sender.enableTransmit(7); // Use **BCM** pin 17
+sender.setRepeatTransmit(15); // Set number of repeat transmissions
+sender.send(sender.switches[0].on)
 console.log('great success')
